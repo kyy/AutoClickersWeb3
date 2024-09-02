8dba65e0dd14f9bbcaeb13bba724dc3b3df60eba
kyy <insider_2012@mail.ru>
2024-09-02 13:51:25 +0300

    delete games

---
+++ games/__diamore.py
@@ -0,0 +1,90 @@
+import logging
+import os
+import time
+
+from arq import cron
+from playwright.async_api import async_playwright, Playwright, Error
+import asyncio
+from fu import start_page_at_phone, create_proccess, multy_tap, get_canonic_full_game_url
+from games.__const import CRON_RUN_AT_STARTUP_URL, CRON_RUN_AT_STARTUP_TAP
+
+NAME = __name__.split('.')[-1]
+TELEGRAM_URL = "https://web.telegram.org/k/#@DiamoreCryptoBot"
+URL = os.getenv(f"{NAME.upper()}_URL")
+
+
+async def run(playwright: Playwright):
+    browser, page = await start_page_at_phone(url=URL, playwright=playwright, timeout=3)
+
+    try:
+        await page.get_by_text('Tap to start game').tap()  # start game
+    except:
+        await browser.close()
+    time.sleep(0.5)
+
+    while True:
+        try:
+            await multy_tap(
+                page=page,
+                semaphore=3,
+                taps=3,
+                locator='//*[@id="root"]/div/div[2]/div[1]/div/div[3]/span',
+            )
+        except:
+            time.sleep(1)
+
+            await browser.close()
+
+
+async def main(ctx=None):
+    try:
+        async with async_playwright() as playwright:
+            await run(playwright)
+    except Error as e:
+        logging.error(f"{NAME} >> {e}")
+
+
+def game():
+    asyncio.run(main())
+
+
+async def process(ctx=None):
+    return create_proccess(target=game)
+
+
+async def refresh_game_url(playwright: Playwright, run=CRON_RUN_AT_STARTUP_URL):
+    """
+    :param playwright:
+    :return: обновляем ссылки игр содержащих временный токен
+    """
+    if run is True:
+        browser, page = await start_page_at_phone(
+            url=TELEGRAM_URL,
+            mobile=False,
+            playwright=playwright,
+            browser_context={"storage_state": "web_telegram.json"},
+        )
+        return await get_canonic_full_game_url(page, browser)
+    elif run is False:
+        return False
+
+
+cron_config: cron = dict(
+    coroutine=process,
+    hour={i for i in range(0, 25, 8)},
+    minute={42},
+    run_at_startup=True,
+    max_tries=3,
+    timeout=2 * 60,
+    unique=True,
+    name=NAME,
+    job_id=f'{NAME}_001',
+)
+
+if __name__ == '__main__':
+    async def main():
+        async with async_playwright() as playwright:
+            await refresh_game_url(playwright)
+
+
+    asyncio.run(main())
--- games/__timefarm.py
+++ games/__timefarm.py
@@ -5,35 +5,26 @@ import time
 from arq import cron
 from playwright.async_api import async_playwright, Playwright, Error
 import asyncio
-from fu import start_page_at_phone, create_proccess, multy_tap, get_canonic_full_game_url
+from fu import start_page_at_phone, create_proccess
 from games.__const import CRON_RUN_AT_STARTUP_URL, CRON_RUN_AT_STARTUP_TAP
 
 NAME = __name__.split('.')[-1]
-TELEGRAM_URL = "https://web.telegram.org/k/#@DiamoreCryptoBot"
+TELEGRAM_URL = "https://web.telegram.org/k/#@TimeFarmCryptoBot"
 URL = os.getenv(f"{NAME.upper()}_URL")
+TAP_PAUSE = 1000
 
 
 async def run(playwright: Playwright):
-    browser, page = await start_page_at_phone(url=URL, playwright=playwright, timeout=3)
-
+    browser, page = await start_page_at_phone(url=URL, playwright=playwright)
     try:
-        await page.get_by_text('Tap to start game').tap()  # start game
+        await page.locator('//*[@id="app"]/div/div[1]/div[4]').tap(force=True)  # claim
+    except:
+        pass
+    time.sleep(5)
+    try:
+        await page.locator('//*[@id="app"]/div/div[1]/div[4]').tap(force=True, timeout=3000)  # claim
     except:
         await browser.close()
-    time.sleep(0.5)
-
-    while True:
-        try:
-            await multy_tap(
-                page=page,
-                semaphore=3,
-                taps=3,
-                locator='//*[@id="root"]/div/div[2]/div[1]/div/div[3]/span',
-            )
-        except:
-            time.sleep(1)
-
-            await browser.close()
 
 
 async def main(ctx=None):
@@ -64,18 +55,31 @@ async def refresh_game_url(playwright: Playwright, run=CRON_RUN_AT_STARTUP_URL):
             playwright=playwright,
             browser_context={"storage_state": "web_telegram.json"},
         )
-        return await get_canonic_full_game_url(page, browser)
+
+        await page.wait_for_selector('xpath=//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[8]')
+
+        await page.locator('//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[8]/div[1]').click()
+        await page.locator(
+            '//*[@id="column-center"]/div/div/div[4]/div/div[1]/div/div[1]/div/div[2]/div/div[2]').click()
+
+        await page.locator(
+            '//*[@class="bubbles-group bubbles-group-last"]/div/div/div[2]/div[1]/button/div').click()  # button
+        await page.locator('xpath=/html/body/div[7]/div/div[2]/button[1]/div').click()  # launch
+        iframe = await page.wait_for_selector('iframe')
+        src = await iframe.get_attribute('src')
+        await browser.close()
+        return src
     elif run is False:
         return False
 
 
 cron_config: cron = dict(
     coroutine=process,
-    hour={i for i in range(0, 25, 8)},
-    minute={42},
-    run_at_startup=CRON_RUN_AT_STARTUP_TAP,
+    hour={i for i in range(0, 25, 4)},
+    minute={45},
+    run_at_startup=True,
     max_tries=3,
-    timeout=2 * 60,
+    timeout=30 * 60,
     unique=True,
     name=NAME,
     job_id=f'{NAME}_001',
