fdabe18f319de58ee78828e3df3cb0ef9ecc550a
kyy <insider_2012@mail.ru>
2024-09-03 10:37:21 +0300

    dogs

---
+++ games/reputationbuilder.py
@@ -0,0 +1,76 @@
+import logging
+import os
+import time
+from arq import cron
+from playwright.async_api import async_playwright, Playwright, Error
+import asyncio
+from fu import start_page_at_phone, create_proccess, multy_tap, get_canonic_full_game_url
+from games.__const import CRON_RUN_AT_STARTUP_TAP, CRON_RUN_AT_STARTUP_URL
+
+NAME = __name__.split('.')[-1]
+TELEGRAM_URL = "https://web.telegram.org/k/#@ReputationBuilderBot"
+URL = os.getenv(f"{NAME.upper()}_URL")
+TAP_PAUSE = 1000
+
+
+async def run(playwright: Playwright):
+    browser, page = await start_page_at_phone(url=URL, playwright=playwright)
+
+    try:
+        await page.get_by_text('start farming').tap()  # закрыть окно с приглашением
+        time.sleep(2)
+    except:
+        pass
+
+
+async def main():
+    try:
+        async with async_playwright() as playwright:
+            await run(playwright)
+    except Error as e:
+        logging.error(f"{NAME} >> {e}")
+
+
+def game():
+    asyncio.run(main())
+
+
+async def process(ctx=None):
+    return create_proccess(target=game)
+
+
+async def refresh_game_url(playwright: Playwright, run=CRON_RUN_AT_STARTUP_URL):
+    """
+    :param playwright:
+    :return: обновляем ссылки игр содержащих временный токен
+    """
+    if run is True:
+        browser, page = await start_page_at_phone(
+            url=TELEGRAM_URL,
+            mobile=False,
+            playwright=playwright,
+            browser_context={"storage_state": "web_telegram.json"},
+        )
+        return await get_canonic_full_game_url(page, browser)
+    elif run is False:
+        return False
+
+
+cron_config: cron = dict(
+    coroutine=process,
+    minute={30},
+    run_at_startup=CRON_RUN_AT_STARTUP_TAP,
+    max_tries=3,
+    timeout=30 * 60,
+    unique=True,
+    name=NAME,
+    job_id=f'{NAME}_001',
+)
+
+if __name__ == '__main__':
+    async def main():
+        async with async_playwright() as playwright:
+            await refresh_game_url(playwright)
+
+
+    asyncio.run(main())
