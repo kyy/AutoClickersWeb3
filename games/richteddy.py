efba93ef7687e37cc7d41dd90af1a2443e8124c0
kyy <insider_2012@mail.ru>
2024-09-20 16:38:40 +0300

    fix

---
+++ games/richteddy.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import time
+from arq import cron
+from playwright.async_api import async_playwright, Playwright, Error
+import asyncio
+from fu import start_page_at_phone, create_proccess, multy_tap
+from games.__const import CRON_RUN_AT_STARTUP_TAP, CRON_RUN_AT_STARTUP_URL
+
+NAME = __name__.split('.')[-1]
+TELEGRAM_URL = "https://web.telegram.org/k/#@token1win_bot"
+URL = os.getenv(f"{NAME.upper()}_URL")
+
+
+async def run(playwright: Playwright):
+    browser, page = await start_page_at_phone(url=URL, playwright=playwright)
+
+    pass
+
+
+async def main():
+    try:
+        async with async_playwright() as playwright:
+            await run(playwright)
+    except Error as e:
+        logging.error(f"{NAME} >> {e}")
+
+
+def game():
+    asyncio.run(main())
+
+
+async def process(ctx=None):
+    return create_proccess(target=game)
+
+
+async def refresh_game_url(playwright: Playwright, run=CRON_RUN_AT_STARTUP_URL):
+    """
+    :param playwright:
+    :return: обновляем ссылки игр содержащих временный токен
+    """
+    if run is True:
+        browser, page = await start_page_at_phone(
+            url=TELEGRAM_URL,
+            mobile=False,
+            playwright=playwright,
+            browser_context={"storage_state": "web_telegram.json"},
+        )
+
+        await page.get_by_role("button").filter(has_text="Играть").last.click()
+        time.sleep(2)
+        await page.get_by_role("button", name="Launch").click()
+
+        time.sleep(2)
+
+        iframe = await page.wait_for_selector('iframe')
+        src = await iframe.get_attribute('src')
+        await browser.close()
+        return src
+    elif run is False:
+        return False
+
+
+cron_config: cron = dict(
+    coroutine=process,
+    hour={i for i in range(0, 25, 4)},
+    minute={12},
+    run_at_startup=True,
+    timeout=30 * 60,
+    unique=True,
+    name=NAME,
+    job_id=f'{NAME}_001',
+)
+
+if __name__ == '__main__':
+    async def main():
+        async with async_playwright() as playwright:
+            await run(playwright)
+
+
+    asyncio.run(main())
