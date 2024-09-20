e26de27c8a07298b5593f96e5ba3752819aa0d80
kyy <insider_2012@mail.ru>
2024-09-20 15:36:28 +0300

    fix

--- games/1wintoken.py
+++ games/1wintoken.py
@@ -10,62 +10,17 @@ from games.__const import CRON_RUN_AT_STARTUP_TAP, CRON_RUN_AT_STARTUP_URL
 NAME = __name__.split('.')[-1]
 TELEGRAM_URL = "https://web.telegram.org/k/#@token1win_bot"
 URL = os.getenv(f"{NAME.upper()}_URL")
-TAP_PAUSE = 1000
 
 
 async def run(playwright: Playwright):
     browser, page = await start_page_at_phone(url=URL, playwright=playwright)
-    try:
-        await page.locator('//*[@id="root"]/div[1]/div/button').tap(force=True, timeout=250)  # continue
-    except:
-        pass
 
-    try:
-        await page.locator('//*[@id="root"]/div[1]/div/button').tap(force=True, timeout=250)  # continue
-    except:
-        pass
-
-    try:
-        await page.locator('//*[@id="root"]/div[1]/div/button').tap(force=True, timeout=250)  # continue
-    except:
-        pass
-
-    try:
-        await page.locator('//*[@id="root"]/div[3]/div/div/div[4]/div/button').tap(force=True,
-                                                                                   timeout=1000)  # get money
-    except:
-        pass
-
-    try:
-        await page.locator('//*[@id="root"]/div[3]/div/div/div[4]/div/button').tap(force=True, timeout=1000)  # close
-    except:
-        pass
-    await asyncio.sleep(0.15)
-    try:
-        await page.locator('//*[@id="root"]/div[3]/div/div/div[4]/div/button').tap(force=True, timeout=1000)  # close
-    except:
-        pass
-
-    while True:
-        count = 0
-        for i in range(TAP_PAUSE):
-            energy_current = await page.locator(
-                'xpath=//*[@id="root"]/div[1]/div/footer/div[1]/div/div/div/div/span[1]').text_content()
-            count += 1
-
-            await multy_tap(
-                page=page,
-                semaphore=10,
-                taps=10,
-                locator='xpath=//*[@id="root"]/div[1]/div/main/div/div[3]'
-            )
-
-            if count == TAP_PAUSE - 1:
-                time.sleep(1)
-            if energy_current == "1":
-                time.sleep(1)
-                await browser.close()
-                return True
+    for _ in range(3):
+        try:
+            await page.get_by_role("button", name="Получить").tap(timeout=500)
+        except:
+            pass
+    await browser.close()
 
 
 async def main():
@@ -115,7 +70,7 @@ cron_config: cron = dict(
     coroutine=process,
     hour={i for i in range(0, 25, 4)},
     minute={10},
-    run_at_startup=CRON_RUN_AT_STARTUP_TAP,
+    run_at_startup=True,
     timeout=30 * 60,
     unique=True,
     name=NAME,
